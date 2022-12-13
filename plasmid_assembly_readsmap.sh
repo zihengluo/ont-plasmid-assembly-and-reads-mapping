22e78d3d9c41d71596c06d5660fe8fb0ad4c4de7
zihengluo <92278842+zihengluo@users.noreply.github.com>
2022-12-13 15:11:36 +1100

    Update plasmid_assembly_readsmap.sh

--- plasmid_assembly_readsmap.sh
+++ plasmid_assembly_readsmap.sh
@@ -2,7 +2,7 @@ eval "$(/opt/conda/bin/conda shell.bash hook)"
 Help()
 {
 echo Here is an automatic script for plasmid assembly and reference vs. reads mapping, please follow the instruction to enter inputs:
-echo In the first place, enter the path to the directory containing subdirectories name after barcode number and have ONT reads in fastq format
+echo In the first place, enter the path to the directory containing fast5 raw data obtained from ONT sequencing
 echo In the second plcae, enter the path to the directory containing reference seuqences name after sample names and are in fasta format
 echo In the third place, enter the path to the headless csv file containing barcode number and sample name
 echo In the fourth place, enter the minimal quality score for filitering
@@ -43,19 +43,19 @@ while read LS;
 do BC=$(echo ${LS} | cut -d ',' -f 1);
 SAMPLE=$(echo ${LS} | cut -d ',' -f 2);
 echo "Working on ${BC} which is sample ${SAMPLE}";
-nextflow run epi2me-labs/wf-clone-validation -r 944a6f9bf6 -profile conda --fastq $1/${BC} --db_directory ${DB_PATH} --out_dir $current_path/$(date +%Y%m%d)>
+nextflow run epi2me-labs/wf-clone-validation -r 944a6f9bf6 -profile conda --fastq $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC} --db_directory ${DB_PATH} --out_dir $current_path/$(date +%Y%m%d)>
 cd $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_assembly_output/$(date +%Y%m%d)_${SAMPLE}_${BC};
 mv ${BC}.final.fasta ${SAMPLE}.${BC}.assembly.fasta;
 mv ${BC}.annotations.bed ${SAMPLE}.${BC}.annotation.bed;
 mv wf-clone-validation-report.html ${SAMPLE}_${BC}_assembly_report.html;
 cd $current_path
-cat $1/${BC}/*.fastq >$1/${BC}/merged.fastq;
+cat $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/*.fastq >$current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq;
 conda activate /home/groups/schwessinger/condaEnvs/NanoplotEtAl;
-NanoFilt -q $4 $1/${BC}/merged.fastq | gzip > $1/${BC}/highQuality-reads.fastq.gz;
+NanoFilt -q $4 $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq | gzip > $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz;
 conda activate plasmid_assembly_readmap;
-minimap2 -ax map-ont -t 10 --secondary=no $2/${SAMPLE}.fasta $1/${BC}/highQuality-reads.fastq.gz | samtools sort -@8 -O BAM -o $current_path/$(date +%Y%m%d)>
+minimap2 -ax map-ont -t 10 --secondary=no $2/${SAMPLE}.fasta $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz | samtools sort -@8 -O BAM -o $current_path/$(date +%Y%m%d)>
 samtools index $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_readmap_output_minQ$4/${BC}.${SAMPLE}.minQ$4.bam;
-rm $1/${BC}/merged.fastq;
-rm $1/${BC}/highQuality-reads.fastq.gz;
+rm $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq;
+rm $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz;
 done<$3
 } | tee $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%T).log
