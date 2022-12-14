93a959517cd61f66c82a7c61ed2733d57fa311a3
zihengluo <92278842+zihengluo@users.noreply.github.com>
2022-12-15 10:47:03 +1100

    Update plasmid_assembly_readsmap.sh

--- plasmid_assembly_readsmap.sh
+++ plasmid_assembly_readsmap.sh
@@ -14,17 +14,19 @@ echo barcode01,PER101
 echo barcode02,PER101
 echo
 echo Here is an example usage:
-echo bash /home/nanopore/plasmid_assembly_readmap/plasmid_assembly_readsmap.sh /media/nvme/MinKNOW/Plasmids/20220825_plasmid1/no_sample/20220825_1335_MN21513_FAR25955_fa2a0187/fast5_subset /home/nanopore/plasmid_assembly_readmap/test_data/referencefa>
+echo bash /home/nanopore/plasmid_assembly_readmap/plasmid_assembly_readsmap.sh /media/nvme/MinKNOW/Plasmids/20220825_plasmid1/no_sample/20220825_1335_MN21513_FAR25955_fa2a0187/fast5 /home/nanopore/plasmid_assembly_readmap/test_data/referencefa /home/nanopore/plasmid_assembly_readmap/test_data/barcode.csv 11
 }
 
-
+# If no input is given, return manual
 if [[ $# -eq 0 ]] ; then
     Help
     exit 0
 fi
 
+# Specify the database path for EPI2ME workflow
 DB_PATH=/home/nanopore/plasmid_assembly_readmap/wf-clone-validation-db
 
+# make  output directories
 current_path=$(pwd)
 mkdir $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output
 mkdir $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq
@@ -39,24 +41,32 @@ mkdir $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m
 
 cd $1 && cd .. && mv fast5 calledFast5 && cd $current_path
 
-
 while read LS;
 do BC=$(echo ${LS} | cut -d ',' -f 1);
 SAMPLE=$(echo ${LS} | cut -d ',' -f 2);
 echo "Working on ${BC} which is sample ${SAMPLE}";
-nextflow run epi2me-labs/wf-clone-validation -r 944a6f9bf6 -profile conda --fastq $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC} --db_directory ${DB_PATH} --out_dir $current_path/$(date +%Y%m%d)>
+
+nextflow run epi2me-labs/wf-clone-validation -r 944a6f9bf6 -profile conda --db_directory ${DB_PATH} \
+	--fastq $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC} \
+	--out_dir $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_assembly_output/$(date +%Y%m%d)_${SAMPLE}_${BC};
+
 cd $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_assembly_output/$(date +%Y%m%d)_${SAMPLE}_${BC};
 mv ${BC}.final.fasta ${SAMPLE}.${BC}.assembly.fasta;
 mv ${BC}.annotations.bed ${SAMPLE}.${BC}.annotation.bed;
 mv wf-clone-validation-report.html ${SAMPLE}_${BC}_assembly_report.html;
-cd $current_path
-cat $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/*.fastq >$current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq;
+sed -i "s/${BC}/${SAMPLE}_${BC}/g" ${SAMPLE}.${BC}.assembly.fasta;
+cd $current_path;
+
+cat $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/*.fastq > $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq;
 conda activate /home/groups/schwessinger/condaEnvs/NanoplotEtAl;
 NanoFilt -q $4 $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq | gzip > $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz;
+
 conda activate plasmid_assembly_readmap;
-minimap2 -ax map-ont -t 10 --secondary=no $2/${SAMPLE}.fasta $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz | samtools sort -@8 -O BAM -o $current_path/$(date +%Y%m%d)>
+minimap2 -ax map-ont -t 10 --secondary=no $2/${SAMPLE}.fasta $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz | samtools sort -@8 -O BAM -o $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_readmap_output_minQ$4/${BC}.${SAMPLE}.minQ$4.bam;
 samtools index $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_readmap_output_minQ$4/${BC}.${SAMPLE}.minQ$4.bam;
+
 rm $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/merged.fastq;
 rm $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%Y%m%d)_calledFastq/${BC}/highQuality-reads.fastq.gz;
 done<$3
 } | tee $current_path/$(date +%Y%m%d)_plasmid_assembly_readmap_output/$(date +%T).log
+
